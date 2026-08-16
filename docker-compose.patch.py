e2c467f2473a6ca6aeb686ae9758fdaf4043d8a9
CodePilot <codepilot@moduforge.dev>
2026-08-16 14:42:05 +0800

    chore: remove 557 non-essential files (debug scripts, binaries, logs, artifacts, backups, process docs)

--- docker-compose.patch.py
+++
@@ -1,4 +0,0 @@
-import re
-with open('docker-compose.yml') as f: c = f.read
-c = c.replace('image: moduforge:latest', 'image: moduforge:patched')
-with open('docker-compose.yml', 'w') as f: f.write(c)
